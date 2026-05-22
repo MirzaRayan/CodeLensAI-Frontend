ff92282d75b9e091110cc6f55bac9b2394fb6e70
app <app@Mirza-Rayan.local>
2026-05-22 15:25:08 +0500

    project setup is done and working on login component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./Components/Home.jsx"
+import Login from "./Components/Login.jsx"
+
+const App = () => {
+  return (
+    <div className="bg-[#F5F5F5]">
+      {/* <Home/>     */}
+      <Login/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Login.jsx
@@ -0,0 +1,14 @@
+import icon from '../assets/'
+
+
+const Login = () => {
+  return (
+    <div className="max-h-screen w-full">
+      <div className="loginForm">
+        <img src={} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Login
